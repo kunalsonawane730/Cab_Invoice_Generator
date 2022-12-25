a8760ae50ebf93ee71673312fddfb120b02c6c8d
Kunal <kunalsonawane730@gmail.com>
2022-12-25 19:46:51 +0530

    Add: creating two different categories for ride normal and premium rides in cab invoice generator program

--- src/test/java/org/bridgelabz/CabInvoiceGeneratorTest.java
+++ src/test/java/org/bridgelabz/CabInvoiceGeneratorTest.java
@@ -8,39 +8,60 @@ import java.util.Arrays;
 public class CabInvoiceGeneratorTest {
     CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
 
+
     @Test
     public void givenDistanceAndTime_ShouldReturnTotalFare() {
         double distance = 2.0;
         int time = 5;
-        double fare = cabInvoiceGenerator.calculateFare(distance, time);
+        double fare = cabInvoiceGenerator.calculateFare(distance, time, "NORMAL");
         Assert.assertEquals(25, fare, 0.0);
     }
+
     @Test
     public void givenLessDistanceAndTime_ShouldReturnMinFare() {
         double distance = 0.1;
         int time = 1;
-        double fare = cabInvoiceGenerator.calculateFare(distance, time);
+        double fare = cabInvoiceGenerator.calculateFare(distance, time, "NORMAL");
         Assert.assertEquals(5, fare, 0.0);
     }
+
     @Test
     public void givenMultipleRides_ShouldReturnInvoiceSummary() {
         Ride[] rides = {
-                new Ride(2.0, 5),
-                new Ride(0.1, 1)
+                new Ride(2.0, 5, "Normal"),
+                new Ride(0.1, 1, "Premium")
         };
         InvoiceSummary invoiceSummary = cabInvoiceGenerator.calculateFare(rides);
-        InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 30.0);
+        InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 35.0);
         Assert.assertEquals(invoiceSummary, expectedInvoiceSummary);
     }
+
     @Test
-    public void givenUserId_ShouldReturnInvoiceSummary() {
+    public void givenUserId_whenValidUser_ShouldReturnInvoiceSummary() {
         Ride[] rides = {
-                new Ride(2.0, 5),
-                new Ride(0.1, 1)
+                new Ride(2.0, 5, "Premium"),
+                new Ride(0.1, 1, "Premium")
         };
         cabInvoiceGenerator.userRideRepository.put("User001", Arrays.asList(rides));
-        InvoiceSummary invoiceSummary = cabInvoiceGenerator.calculateFare("User001");
-        InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 30.0);
-        Assert.assertEquals(invoiceSummary, expectedInvoiceSummary);
+        try {
+            InvoiceSummary invoiceSummary = cabInvoiceGenerator.calculateFare("User001");
+            InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 50.0);
+            Assert.assertEquals(invoiceSummary, expectedInvoiceSummary);
+        } catch (CabInvoiceCustomException e) {}
+    }
+
+    @Test
+    public void givenUserId_whenInValidUser_ShouldThrowException() {
+        Ride[] rides = {
+                new Ride(2.0, 5, "Premium"),
+                new Ride(0.1, 1, "Premium")
+        };
+        cabInvoiceGenerator.userRideRepository.put("User001", Arrays.asList(rides));
+        try {
+            InvoiceSummary invoiceSummary = cabInvoiceGenerator.calculateFare("User002");
+            InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 50.0);
+        } catch (CabInvoiceCustomException e) {
+            Assert.assertEquals(CabInvoiceCustomException.ExceptionType.INVALID_USER, e.type);
+        }
     }
 }
